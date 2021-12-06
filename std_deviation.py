20b9effaff8e510cbe1d6aee68676bcb786a78bc
Ishan1818 <72986416+Ishan1818@users.noreply.github.com>
2021-12-06 20:00:19 +0530

    Add files via upload

---
+++ std_deviation.py
@@ -0,0 +1,37 @@
+import csv
+import pandas as pd
+import plotly.express as px
+import math
+
+with open('data.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+#sorting the data to get the list
+data=file_data[0]
+#finding mean
+def mean(data):
+    n= len(data)
+    total =0
+    for x in data:
+        total += int(x)
+
+    mean = total / n
+    return mean
+
+# squaring and getting the values
+
+
+squared_list= []
+for number in data:
+    a = int(number) - mean(data)
+    a= a**2
+    squared_list.append(a)
+#getting sum
+sum=0
+for i in squared_list:
+    sum=sum+i
+#dividing the sum by total values
+result=sum/(len(data)-1)
+#finding square root
+std_deviation=math.sqrt(result)
+print(std_deviation)
